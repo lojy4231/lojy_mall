e332c6564e3ae3969609f41eff77bdd40fa3847d
lojy4231 <lojy4231@gmail.com>
2022-05-23 09:18:54 +0900

    미들웨어 생성

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // express 모듈을 사용하겠다.
+const app = express(); // express에 객체를 받아 옴.
 const port = 3000;
+
+app.use((req, res, next) => {
+    console.log("미들웨어가 구현됐나?");
+    next(); // next는 다음 미들웨어로 넘어 갈 수 있게 해준다.
+});
 
 app.get("/", (req, res) =>{
     res.send("Hello World");
 });
 
 app.listen(port, () =>{
-    console.log(port, "포트 서버가 켜졌어요")
+    console.log(port, "포트로 서버가 켜졌어요")
 });
