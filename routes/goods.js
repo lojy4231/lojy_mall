a3e38f5b4f8156ab6e9c1dbda890ca36b5f7165c
lojy4231 <lojy4231@gmail.com>
2022-05-23 15:46:04 +0900

    장바구니 목록에 상품 삭제, 수량변경 추가

--- routes/goods.js
+++ routes/goods.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const Goods = require("../schemas/goods")
+const Goods = require("../schemas/goods");
+const Cart = require("../schemas/cart");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -24,6 +25,46 @@ router.get("/goods/:goodsId", async (req, res) => {
     res.json({
         detail,
     });
+});
+
+
+
+router.post("/goods/:goodsId/cart", async (req, res) => {
+    const { goodsId } = req.params;
+    const { quantity } = req.body;
+
+    const existCarts = await Cart.find({ goodsId: Number(goodsId) });
+    if(existCarts.length){
+        return res.status(400).json({ success: false, errorMessage: "이미 장바구니에 있는 상품입니다."});
+    }
+
+    await Cart.create({ goodsId: Number(goodsId), quantity });
+    res.json({ success: true });
+
+});
+
+router.delete("/goods/:goodsId/cart", async (req, res) => {
+    const { goodsId } = req.params;
+
+    const existCarts = await Cart.find({ goodsId: Number(goodsId) });
+    if(existCarts.length){
+        await Cart.deleteOne({ goodsId: Number(goodsId )});
+    }
+
+    res.json({ success: true });
+});
+
+router.put("/goods/:goodsId/cart", async (req, res) => {
+    const { goodsId } = req.params;
+    const { quantity } = req.body;
+
+    const existCarts = await Cart.find({ goodsId: Number(goodsId) });
+    if(existCarts.length){
+        await Cart.updateOne({ goodsId: Number(goodsId) }, { $set: { quantity }});
+    }
+
+    res.json({ success: true });
+    
 });
 
 router.post("/goods", async (req, res) => {
