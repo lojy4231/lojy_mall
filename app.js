8c30e8500e8db97ca3b1bc367a069d1c49379c3a
lojy4231 <lojy4231@gmail.com>
2022-05-23 09:31:50 +0900

    미들웨어에 로그 남기기 추가

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express"); // express 모듈을 사용하겠다.
 const app = express(); // express에 객체를 받아 옴.
 const port = 3000;
 
-app.use((req, res, next) => {
-    console.log("미들웨어가 구현됐나?");
-    next(); // next는 다음 미들웨어로 넘어 갈 수 있게 해준다.
-});
+const requestMiddleware = (req, res, next) => {
+    console.log("Request URL:", req.originalUrl, "-", new Date());
+    next();
+};
+
+app.use(requestMiddleware);
+// app.use((req, res, next) => {
+//     console.log("미들웨어가 구현됐나?");
+//     next(); // next는 다음 미들웨어로 넘어 갈 수 있게 해준다.
+// });
 
 app.get("/", (req, res) =>{
     res.send("Hello World");
