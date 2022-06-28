76623477d5a9e1b85a85d82afab1b1fd780d2b1c
lojy4231 <lojy4231@gmail.com>
2022-06-28 09:55:20 +0900

    전체 업데이트

--- schemas/index.js
+++ schemas/index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
+
+console.log("DB_ID:", process.env.DBID);
+console.log("DB_PW:", process.env.DBPW);
 
 const connect = () =>{
     mongoose.connect("mongodb://localhost:27017/lojy_mall", { ignoreUndefined: true }).catch((err) =>{
